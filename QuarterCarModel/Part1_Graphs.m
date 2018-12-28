b7f52302cb386d940a935af7d3dce3efeeed063c
betts-jt <01betjos@gmail.com>
2018-12-28 16:22:43 +0000

    Add code to plot all graphs for body model

--- QuarterCarModel/Part1_Graphs.m
+++ QuarterCarModel/Part1_Graphs.m
@@ -1,12 +1,29 @@
 %% Body displacement Tests
+a=0;
 [a, b] = sim('BodyModel');
 figure(1)
 plot(a,b(:,1))
 xlabel('Time, s')
 ylabel('Displacement, m')
 
+
 figure(2)
 plot(a,b(:,2))
 xlabel('Time, s')
 ylabel('Velocity, m/s')
 
+a=1;
+[a, b] = sim('BodyModel');
+figure(3)
+plot(a,b(:,1))
+xlabel('Time, s')
+ylabel('Displacement, m')
+
+
+figure(4)
+plot(a,b(:,2))
+xlabel('Time, s')
+ylabel('Velocity, m/s')
+
+%% Suspention model tests
+figure(5)
