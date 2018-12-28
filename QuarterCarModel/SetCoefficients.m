46f158cacb37d02b67d5658f0e56847536769764
betts-jt <01betjos@gmail.com>
2018-12-28 16:02:36 +0000

    Update SetCoefficients.m

--- QuarterCarModel/SetCoefficients.m
+++ QuarterCarModel/SetCoefficients.m
@@ -1,5 +1,7 @@
+clear
 m1 = 250; % 1/4 body mass. kg
 m2 = 20; % Tyre mass. kg
 ks = 2e4; % Suspention stiffness. N/m
 cs = 1000; % Suspention Damping. Ns/m
 kt = 14e4; % Tyre Stiffness. N/m
+g = 9.81; % Acceleration due to gravity. m/s^2
