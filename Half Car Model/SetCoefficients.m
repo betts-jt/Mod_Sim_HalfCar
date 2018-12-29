c6faa4accece1ab49edc7ac84327bad13b282d23
betts-jt <01betjos@gmail.com>
2018-12-29 17:52:10 +0000

    add initial displacements calcualtions

--- Half Car Model/SetCoefficients.m
+++ Half Car Model/SetCoefficients.m
@@ -6,7 +6,7 @@ mb = 500; % Mass of 1/2 car. kg
 a = 1.25; % Distance of front wheel from 1/4 car center of mass. m
 b = 1.4; % Distance of rear wheel from 1/4 car center of mass. m
 
-v = 10; % Car speed. m/s
+v = 1; % Car speed. m/s
 
 % Check if speed = 0 to avoid an infinate delay
 if v == 0
@@ -14,6 +14,7 @@ if v == 0
 else
     delay = (a+b)/v;
 end
+
 %% 1/4 car coeffcients
 m1 = mb/2; % 1/4 body mass. kg
 m2 = 20; % Tyre mass. kg
@@ -30,3 +31,27 @@ g = 9.81; % Acceleration due to gravity. m/s^2
 x1 = 0.2; % Extention at which the spring stiffness increases. m 
 x2 = 0.5; % Limit extention for spring look up table. m
 
+%% Initial suspention displacements
+% These are needed so that the car doesn't fall onto its suspention at the
+% start of each test. Set all of these values to 0 for verification tests
+
+% Calculating the force at the rear and front suspention due to mass
+Fr = (mb*g*a)/(a+b); % Force at rear suspention. N
+Ff = mb*g - Fr; % Force at front suspention. N
+
+% Calculate wheel deflection due to these forces
+FrontWheelDisp = -(Ff+mb)/kt; % Front wheel initial displacement. m
+RearWheelDisp = -(Fr+mb)/kt; % Rear wheel initial displacement. m
+
+%Calcualte Initial Body Diplacements
+FrontBodyDisp = -(Ff+m2*g)/kt - Ff/ks; % Front body initial displacement. m
+COMBodyDisp = -((mb+2*m2)*g)/(2*kt) - (mb*g)/(2*ks); % COM body initial displacement. m
+RearBodyDisp = -(Fr+m2*g)/kt - Fr/ks; % Rear body initial displacement. m
+
+%% Coeficients used for Sine wave test road
+amplitude = 0.01; % Amplitude of road surface. m
+lambda = 1; % Wave length of road. m
+
+Period = lambda/v; % Period of sine wave. s
+Fre = 1/Period; % Frequency of sine wave. Hz
+Fre_r = Fre*2*pi; % Frequency of sine wave. Hrad/s
