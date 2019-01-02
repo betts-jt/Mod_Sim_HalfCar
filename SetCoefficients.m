4da49fecfd7df89f6a70a4c296a1c8f212789303
betts-jt <01betjos@gmail.com>
2019-01-02 12:48:53 +0000

    Edit to allow both full and half car models to be run

--- SetCoefficients.m
+++ SetCoefficients.m
@@ -12,7 +12,7 @@ s = 3; % Width of car. m
 c = 1.3; % Distance of COM to left wheels. m
 d = 1.7; % Distance of COM to right wheels. m
 
-v = 10; % Car speed. m/s
+v = 5; % Car speed. m/s
 
 % Check if speed = 0 to avoid an infinate delay
 if v == 0
@@ -22,11 +22,14 @@ else
 end
 
 %% 1/4 car coeffcients
+mbh = mb/2; % Half body mass. kg
 m1 = mb/4; % 1/4 body mass. kg
 m2 = 20; % Tyre mass. kg
 
 ks = 2e4; % Suspention stiffness. N/m
-ksstiff = 20*ks; % Hardened supention spring stiffness. N/n
+ksstiff = 4e5; % Hardened supention spring stiffness. N/n
+ksSteel = 6.6e9; % Steel stiffness of suspention bump stop
+
 kt = 14e4; % Tyre Stiffness. N/m
 
 csc = 600; % Suspention Damping Compression. Ns/m
@@ -35,8 +38,9 @@ cse = 1200; % Suspention Damping Extention. N/m
 g = 9.81; % Acceleration due to gravity. m/s^2
 
 x1 = 0.2; % Extention at which the spring stiffness increases. m 
-x2 = 0.5; % Limit extention for spring look up table. m
+x2 = 0.25; % maximum spring displacement either positive or negative. m
 
+xt_Lim = 0.1; % Limit of tyre extention. m
 %% Coeficients used for Sine wave test road
 amplitude = 0.01; % Amplitude of road surface. m
 lambda = 1; % Wave length of road. m
