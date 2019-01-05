a61bb12ec8585b50ef3f311e72c805f5b2b2283d
betts-jt <01betjos@gmail.com>
2019-01-05 14:12:34 +0000

    Update for report

--- Half Car Model Creation/SetCoefficients_HC.m
+++ Half Car Model Creation/SetCoefficients_HC.m
@@ -1,8 +1,13 @@
+% SetCoefficients_HC.m
+%   Sets the values for the coefficients and masses needed for the simulink
+%   Half car model
+
 clear
 
 %% 1/2 Car coefficients
+
 I = 600; % Second moment of area of 1/2 car. kg/m^2
-mb = 500; % Mass of 1/2 car. kg
+mbh = 500; % Mass of 1/2 car. kg
 a = 1.25; % Distance of front wheel from 1/4 car center of mass. m
 b = 1.4; % Distance of rear wheel from 1/4 car center of mass. m
 
@@ -16,20 +21,25 @@ else
 end
 
 %% 1/4 car coeffcients
-m1 = mb/2; % 1/4 body mass. kg
+% Masses
+m1 = mbh/2; % 1/4 body mass. kg
 m2 = 20; % Tyre mass. kg
 
+% Spring Coefficients
 ks = 2e4; % Suspention stiffness. N/m
 ksstiff = 20*ks; % Hardened supention spring stiffness. N/n
 kt = 14e4; % Tyre Stiffness. N/m
 
+x1 = 0.2; % Extention at which the spring stiffness increases. m 
+x2 = 0.5; % Limit extention for spring look up table. m
+
+%Damping Coefficients
 csc = 600; % Suspention Damping Compression. Ns/m
 cse = 1200; % Suspention Damping Extention. N/m
 
+%Universal Coefficients
 g = 9.81; % Acceleration due to gravity. m/s^2
 
-x1 = 0.2; % Extention at which the spring stiffness increases. m 
-x2 = 0.5; % Limit extention for spring look up table. m
 
 %% Coeficients used for Sine wave test road
 amplitude = 0.01; % Amplitude of road surface. m
